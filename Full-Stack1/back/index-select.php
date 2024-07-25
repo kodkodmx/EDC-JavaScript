8111d04f16ac6df83c4af3b07d3967835aea017a
Alberto Haboba <betoh@hotmail.com>
2024-07-24 20:14:26 -0600

    nombre de base de datos con guin bajo

--- Full-Stack1/back/index-select.php
+++ Full-Stack1/back/index-select.php
@@ -4,7 +4,7 @@ header( "Access-Control-Allow-Origin: *" );
 $db_host = "localhost";
 $db_user = "root";
 $db_pass = "";
-$db_name = "js-edc";
+$db_name = "js_edc";
 
 $conn = mysqli_connect( $db_host, $db_user, $db_pass, $db_name );
 if( ! $conn ) { die( "DB Connection failed..!" ); }
