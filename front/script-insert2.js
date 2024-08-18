448969d91c85683baf845e1bff8850a973400353
Alberto Haboba <alberto.haboba@gmail.com>
2024-08-18 11:24:42 -0600

    listo insert2 con resultado que esaparece

--- front/script-insert2.js
+++ front/script-insert2.js
@@ -1,34 +1,30 @@
 var formUI 			= document.getElementsByTagName( "form" )[ 0 ];
 var inputsUI 		= formUI.getElementsByTagName( "input" );
 var holder 			= document.getElementById( "holder" );
-var dataProperty  	= inputsUI[ 2 ].value; 
-var dataLocation  	= inputsUI[ 3 ].value; 
-var dataPrice 	  	= inputsUI[ 4 ].value; 
-var submit			= inputsUI[ 5 ];
 
 
 submit.addEventListener( "click", function( e ) {
 	e.preventDefault();
-	holder.innerHTML = "";
-
-
-
-var endpoint = "http://edc-javascript.dvl.to/";
-var xhr = new XMLHttpRequest();
-xhr.open( "POST", endpoint, true );
-xhr.setRequestHeader( "Content-Type", "application/x-www-form-urlencoded" );
-xhr.send( "property=" + dataProperty + "&location=" + dataLocation + "&price=" + dataPrice );
-xhr.addEventListener( "load", loadInsertedData );
+	var dataProperty  	= inputsUI[ 2 ].value; 
+	var dataLocation  	= inputsUI[ 3 ].value; 
+	var dataPrice 	  	= inputsUI[ 4 ].value; 
+	var submit			= inputsUI[ 5 ];
+	var endpoint = "http://edc-javascript.dvl.to/";
+	var xhr = new XMLHttpRequest();
+	xhr.open( "POST", endpoint, true );
+	xhr.setRequestHeader( "Content-Type", "application/x-www-form-urlencoded" );
+	xhr.send( "property=" + dataProperty + "&location=" + dataLocation + "&price=" + dataPrice );
+	xhr.addEventListener( "load", loadInsertedData );
 
 });
 
 function loadInsertedData( e ) {
 	var dataJson = JSON.parse( e.target.responseText );
 	formUI.reset();
-	dataProperty="";
-	dataLocation="";
-	dataPrice="";
 	holder.innerHTML = dataJson.status;
+	setTimeout( function() {
+		holder.innerHTML = "";
+	}, 3000 );
 };
 
 
